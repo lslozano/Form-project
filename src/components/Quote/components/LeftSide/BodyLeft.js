d56563570663a6f29d57b584bbbd0864918d2e0f
Luis Lozano <lslozano11@gmail.com>
2021-07-22 19:15:47 -0500

    Move the logic to create the delivery ranges to a method and call it with a return value

--- src/components/Quote/components/LeftSide/BodyLeft.js
+++ src/components/Quote/components/LeftSide/BodyLeft.js
@@ -4,33 +4,24 @@ const BodyLeft = ({ shippingChannel }) => {
   let deliveryStartRange;
   let deliveryEndRange;
 
-  if (shippingChannel === 'Air') {
-    const minimumDaysForStartRange = Math.ceil(3);
-    const maximumDaysForStartRange = Math.floor(7);
+  const determineQuoteDayRanges = (shippingChannel) => {
+    const minimumDaysForStartRange = shippingChannel === 'Air' ? Math.ceil(3) : Math.ceil(25);
+    const maximumDaysForStartRange = shippingChannel === 'Air' ? Math.floor(7) : Math.ceil(30);
 
-    const minimumDaysForEndRange = Math.ceil(2);
-    const maximumDaysForEndRange = Math.ceil(4);
-
-    const daysToAddToStartRange = Math.floor(Math.random() * (maximumDaysForEndRange - minimumDaysForEndRange + 1) + minimumDaysForEndRange);
-
-    deliveryStartRange = Math.floor(Math.random() * (maximumDaysForStartRange - minimumDaysForStartRange + 1) + minimumDaysForStartRange);
-    deliveryEndRange = deliveryStartRange + daysToAddToStartRange;
-  } else if (shippingChannel === 'Ocean') {
-    const minimumDaysForStartRange = Math.ceil(25);
-    const maximumDaysForStartRange = Math.floor(30);
-
-    const minimumDaysForEndRange = Math.ceil(5);
-    const maximumDaysForEndRange = Math.ceil(10);
+    const minimumDaysForEndRange = shippingChannel === 'Air' ? Math.ceil(2) : Math.ceil(5);
+    const maximumDaysForEndRange = shippingChannel === 'Air' ? Math.ceil(4) : Math.ceil(10);
 
     const daysToAddToStartRange = Math.floor(Math.random() * (maximumDaysForEndRange - minimumDaysForEndRange + 1) + minimumDaysForEndRange);
 
     deliveryStartRange = Math.floor(Math.random() * (maximumDaysForStartRange - minimumDaysForStartRange + 1) + minimumDaysForStartRange);
     deliveryEndRange = deliveryStartRange + daysToAddToStartRange;
+    
+    return  `${deliveryStartRange}-${deliveryEndRange}`;
   }
 
   return (
     <div className="body-left-container">
-      <h6 className="body-left-header">{deliveryStartRange}-{deliveryEndRange} days</h6>
+      <h6 className="body-left-header">{determineQuoteDayRanges(shippingChannel)} days</h6>
       <section className="body-left-section">
         <p className="body-left-text">Estimated delivery</p>
         <p className="body-left-date">Sept 20 - Sept 26</p>
